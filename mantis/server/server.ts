2f88884ae44569a4c12366ceb656a52323b0fea7
DonnaIsabel97 <tpr.th.bvb@gmail.com>
2025-02-07 20:54:19 -0500

    Connection to express in docker fix it

--- mantis/server/server.ts
+++ mantis/server/server.ts
@@ -73,4 +73,4 @@ app.post('/track-metrics', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3001, () => console.log('Server running on port 3001'));
