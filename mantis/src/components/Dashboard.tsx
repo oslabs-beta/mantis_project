4e98475174e3e202c7d81f5ba60b4c20dd4e38b3
Christina Ju <christinajj314@gmail.com>
2025-02-11 11:57:56 -0800

    testing if rebase aborted

--- mantis/src/components/Dashboard.tsx
+++ mantis/src/components/Dashboard.tsx
@@ -25,7 +25,7 @@ const Dashboard: React.FC = () => {
   const grafanaUrl =
     viewType === 'overview'
       ? `http://localhost:3000/d-solo/fecp0c24oog74b/data-from-database?orgId=1&timezone=browser&panelId=1&__feature.dashboardSceneSolo`
-      : `http://localhost:3000/d-solo/fecp0c24oog74b/data-from-database?orgId=1&timezone=browser&panelId=1&__feature.dashboardSceneSolo`;
+      : ``;
   return (
     <div>
       <h1>Dashboard</h1>
@@ -38,7 +38,7 @@ const Dashboard: React.FC = () => {
             src='http://localhost:3000/d-solo/fecp0c24oog74b/data-from-database?orgId=1&timezone=browser&panelId=1&__feature.dashboardSceneSolo'
             width='450'
             height='200'
-            frameBorder='0'
+            // frameborder='0'
             title='overview'
           ></iframe>
         ) : (
@@ -46,7 +46,7 @@ const Dashboard: React.FC = () => {
             src='http://localhost:3000/d-solo/fecp0c24oog74b/data-from-database?orgId=1&timezone=browser&panelId=1&__feature.dashboardSceneSolo'
             width='450'
             height='200'
-            frameBorder='0'
+            // frameborder='0'
             title='single'
           ></iframe>
         )}
